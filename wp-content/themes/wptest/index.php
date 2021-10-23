dc5ac0160126ea30f18f37b4904c5200ded06ef6
Evgeny <tungusv89@mail.ru>
2021-10-23 10:43:31 +0500

    wp

---
+++ wp-content/themes/wptest/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ) ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php the_title() ?></title>
+    <?php wp_head() ?>
+</head>
+<body>
+    <header>
+        <div class="container">
+            header
+        </div>
+    </header>
+    <div class="container">
+            main
+    </div>
+    <footer>
+        <div class="container">
+            footer
+        </div>
+    </footer>
+</body>
+</html>
